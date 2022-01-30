858e26f63ac9e0d6a59b48b3e6613547ac226bd2
Bruno Roman <Bruno Roman>
2022-01-30 06:53:31 -0800

    Service layer, component registration

--- src/main/java/br/com/brunoweb/course/repositories/UserRepository.java
+++ src/main/java/br/com/brunoweb/course/repositories/UserRepository.java
@@ -4,8 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.brunoweb.course.entities.User;
 
+
 public interface UserRepository extends JpaRepository<User, Long>{
 	
-	
-
 }
