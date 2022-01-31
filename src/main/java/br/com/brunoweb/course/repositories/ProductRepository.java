cdf9bef559002f7e95ba7fc5923bfc8bc84768aa
Bruno Roman <Bruno Roman>
2022-01-31 07:44:47 -0800

    product

---
+++ src/main/java/br/com/brunoweb/course/repositories/ProductRepository.java
@@ -0,0 +1,10 @@
+package br.com.brunoweb.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.brunoweb.course.entities.Product;
+
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+	
+}
